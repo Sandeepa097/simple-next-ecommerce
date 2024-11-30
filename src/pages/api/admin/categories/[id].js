b5200b1a4c6cfd64bcecfcd4aae686a077f51b18
Gihan Sandeepa <gihan4880@gmail.com>
2024-11-30 18:47:34 +0530

    Minor fixes on allowed methods

--- src/pages/api/admin/categories/[id].js
+++ src/pages/api/admin/categories/[id].js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
       break;
 
     default:
-      res.setHeader('Allow', ['PUT', 'DELETE']);
+      res.setHeader('Allow', ['GET', 'PUT', 'DELETE']);
       res.status(405).end(`Method ${method} Not Allowed`);
   }
 }
